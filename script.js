12c1692b0bad7167078989e6bc3995179970628a
marlbotsftc3526 <75097875+marlbotsftc3526@users.noreply.github.com>
2026-09-25 20:42:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -625,7 +625,7 @@ const projectsData = [
         season: '25-26',
         country: 'France',
         description: ' ',
-        tags: ["Into the Deep"],
+        tags: ["Decode"],
         links: [
             { text: 'Google Drive', url: 'https://drive.google.com/file/d/1DvmzXrwTwdSR4s2S0yO9oNmaqk0oBaFA/view', icon: 'fas fa-cube' }
               ]
@@ -639,7 +639,7 @@ const projectsData = [
         season: '25-26',
         country: 'Westford, Massachusetts',
         description: ' ',
-        tags: ["Into the Deep"],
+        tags: ["Decode"],
         links: [
             { text: 'Onshape', url: 'https://cad.onshape.com/documents/7573300c60aec127cf587728/w/96f139eb25cbe55565a1fb2b/e/a7102a7ddccdcdd7951b32c4', icon: 'fas fa-cube' }
                 ]
@@ -653,7 +653,7 @@ const projectsData = [
         season: '25-26',
         country: 'Irvine, California',
         description: ' ',
-        tags: ["Into the Deep"],
+        tags: ["Decode"],
         links: [
             { text: 'Onshape', url: 'https://cad.onshape.com/documents/c1e442ac6f370555984182d5/w/18917fa871bbc603bc7e6d36/e/ca72e1a1b4724fb84b28cdd3', icon: 'fas fa-cube' }
             ]
